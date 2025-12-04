9b16ade8306780da4165feefa434541c5c334697
Emiliano <emyquaranta@gmail.com>
2025-12-04 17:40:46 +0100

    Add files via upload

--- generate_variables.m
+++ generate_variables.m
@@ -1,31 +1,31 @@
-function [M, q, l, u, a, b, x0] = generate_variables(n)
+function [Q, q, l, u, a, b, x0] = generate_variables(n)
 
  A = randn(n);
- M = A' * A;
- q = randn(n,1); %sempre valido
+ Q = A' * A;
+ q = randn(n,1); 
  LOW  = 1;
  HIGH =  10;
 
-% generare i bound inferiori
+% genero i bound inferiori
  l = LOW + (HIGH-LOW) * rand(n,1);
 
-% generare i bound superiori, sempre > l
- u = l + 0.5 + rand(n,1);   % così u_i > l_i garantito
- a = rand(n,1);  % tutti positivi → più stabile
+% genero i bound superiori, sempre > l
+ u = l + 0.5 + rand(n,1); 
+ a = rand(n,1); 
 
- % calcola range valido
+ % calcolo range valido
  b_min = a' * l;
  b_max = a' * u;
 
  % scegli b in modo "random" dentro l'intervallo possibile
  b = b_min + (b_max - b_min) * rand();
- t = (b - a'*l) / (a'*(u-l));   % scala tra 0 e 1
-t = max(0, min(1, t));          % assicura 0 <= t <= 1
+ t = (b - a'*l) / (a'*(u-l));  
+t = max(0, min(1, t));          
 x0 = l + t*(u - l);             % x0 nel box, soddisfa il vincolo
  disp("n:")
  disp(n)
- disp("M:")
- disp(M)
+ disp("Q:")
+ disp(Q)
  disp("q:")
  disp(q)
  disp("l:")
