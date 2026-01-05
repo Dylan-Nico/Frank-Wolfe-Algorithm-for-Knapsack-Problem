bd7b93e5ce47b0e7fd422573003e1b6732716ed3
Dylan Nico Ambrosi <81829336+Dylan-Nico@users.noreply.github.com>
2026-01-05 18:03:54 +0100

    Delete warning_bound.m

--- warning_bound.m
+++
@@ -1,9 +0,0 @@
-function warning_bound(tolBound, x, l, u)
-
-if any(abs(x - l) < tolBound)
-        fprintf('WARNING: touching LOWER bound\n');
-end
-
-if any(abs(x - u) < tolBound)
-        fprintf('WARNING: touching UPPER bound\n');
-end
