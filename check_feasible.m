f3e1e20d9408b2e0a4b5c689abc3738fb93bcf83
Dylan Nico Ambrosi <81829336+Dylan-Nico@users.noreply.github.com>
2026-01-05 18:19:36 +0100

    Delete check_feasible.m

--- check_feasible.m
+++
@@ -1,6 +0,0 @@
-function ok = check_feasible(x, a, b, l, u)
-% Check if x is within the bounds defined by l and u and if linear
-% constraint is valid
-    tol = 1e-9;
-    ok = (a' * x >= b - tol) && all(x >= l - tol) && all(x <= u + tol);
-end
